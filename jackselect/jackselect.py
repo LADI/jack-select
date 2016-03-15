8b80ddcf19a8a0c7bc73011247765174fd0a972a
Christopher Arndt <chris@chrisarndt.de>
2016-03-15 14:59:13 +0100

    Make JACK status/stats retrieval asynchroneous

--- jackselect/jackselect.py
+++ jackselect/jackselect.py
@@ -2,14 +2,13 @@
 """A systray app to set the JACK configuration from QJackCtl presets via DBus.
 """
 
-import io
 import logging
 import os
 import sys
 
-os.environ['NO_AT_BRIDGE'] = "1"
+os.environ['NO_AT_BRIDGE'] = "1"  # noqa
 import gi
-gi.require_version('Gtk', '3.0')
+gi.require_version('Gtk', '3.0')  # noqa
 from gi.repository import Gtk, GObject
 from gi.repository.GdkPixbuf import Pixbuf
 
@@ -17,7 +16,8 @@ from gi.repository.GdkPixbuf import Pixbuf
 from pkg_resources import resource_filename
 from xdg import BaseDirectory as xdgbase
 
-from .jackcontrol import JackCfgInterface, get_jack_controller
+from .jackcontrol import (JackCfgInterface, JackCtlInterface,
+                          get_jack_controller)
 from .qjackctlconf import get_qjackctl_presets
 
 
@@ -107,11 +107,12 @@ class JackSelectApp:
         self.gui = Indicator('jack.png')
         self.gui.icon.set_has_tooltip(True)
         self.gui.icon.connect("query-tooltip", self.tooltip_query)
-        self.jack_is_started = None
-        self.status = "No status available"
+        self.jack_status = {}
+        self.tooltext = "No status available."
         self.jackdbus = None
-        self.jackctl = get_jack_controller()
-        self.jackcfg = JackCfgInterface(self.jackctl)
+        dbus_obj = get_jack_controller()
+        self.jackctl = JackCtlInterface(dbus_obj)
+        self.jackcfg = JackCfgInterface(dbus_obj)
         self.load_presets()
         GObject.timeout_add(1000, self.load_presets)
         GObject.timeout_add(500, self.check_jack_status)
@@ -144,42 +145,57 @@ class JackSelectApp:
         self.gui.add_separator()
         self.menu_stop = self.gui.add_menu_item(
             self.stop_jack_server, "Stop JACK Server", icon='stop.png')
-        self.menu_stop.set_sensitive(bool(self.jack_is_started))
+        self.menu_stop.set_sensitive(bool(self.jack_status.get('is_started')))
         self.gui.add_separator()
         self.menu_quit = self.gui.add_menu_item(
             lambda x: Gtk.main_quit(), "Quit",
             icon='quit.png')
         self.gui.menu.show_all()
 
-    def check_jack_status(self):
-        if self.jackctl:
-            if self.jackctl.IsStarted():
-                sr = self.jackctl.GetSampleRate()
-                bufsize = self.jackctl.GetBufferSize()
-                load = self.jackctl.GetLoad()
-                xruns = self.jackctl.GetXruns()
-                latency = self.jackctl.GetLatency()
-                self.status = (
-                    "%i Hz, %i frames (%0.1f ms), %i%% load, %i xruns" %
-                    (sr, bufsize, latency, load, xruns))
-
-                if not self.jack_is_started:
+    def receive_status(self, value, name=None):
+        if name == 'is_started':
+            if value != self.jack_status.get('is_started'):
+                if value:
                     self.gui.set_icon('started.png')
                     log.info("JACK server started.")
-                    self.jack_is_started = True
-            elif self.jack_is_started in (True, None):
-                self.gui.set_icon('stopped.png')
-                log.info("JACK server stopped.")
-                self.status = "JACK server stopped."
-                self.jack_is_started = False
+                    self.menu_stop.set_sensitive(True)
+                else:
+                    self.gui.set_icon('stopped.png')
+                    log.info("JACK server stopped.")
+                    self.tooltext = "JACK server stopped."
+                    self.menu_stop.set_sensitive(False)
 
-        self.menu_stop.set_sensitive(self.jack_is_started)
+        self.jack_status[name] = value
+
+        if self.jack_status.get('is_started'):
+            try:
+                self.tooltext = (
+                    "%(samplerate)i Hz, %(period)i frames "
+                    "(%(latency)0.1f ms), %(load)i%% load, %(xruns)i xruns" %
+                    self.jack_status)
+            except KeyError:
+                self.tooltext = "No status available."
+
+    def check_jack_status(self):
+        if self.jackctl:
+            self.jackctl.is_started(self.receive_status)
+            # Get stats later, after we received running status
+            GObject.timeout_add(250, self.get_stats)
+
         return True  # keep function scheduled
+
+    def get_stats(self):
+        if self.jack_status.get('is_started'):
+            self.jackctl.get_sample_rate(self.receive_status)
+            self.jackctl.get_period(self.receive_status)
+            self.jackctl.get_load(self.receive_status)
+            self.jackctl.get_xruns(self.receive_status)
+            self.jackctl.get_latency(self.receive_status)
 
     def tooltip_query(self, widget, x, y, keyboard_mode, tooltip):
         """Set tooltip for the systray icon."""
         if self.jackctl:
-            tooltip.set_text(self.status)
+            tooltip.set_text(self.tooltext)
         else:
             tooltip.set_text("No JACK-DBus connection")
 
@@ -206,29 +222,23 @@ class JackSelectApp:
             GObject.timeout_add(1000, self.start_jack_server)
 
     def start_jack_server(self, *args, **kwargs):
-        if self.jackctl and not self.jackctl.IsStarted():
-            try:
-                self.jackctl.StartServer()
-            except Exception as exc:
-                log.error("Failed to start JACK: %s", exc)
-                self.status = str(exc)
+        if self.jackctl and not self.jack_status.get('is_started'):
+            self.jackctl.start_server()
             self.check_jack_status()
 
     def stop_jack_server(self, *args, **kwargs):
-        if self.jackctl and self.jackctl.IsStarted():
-            try:
-                self.jackctl.StopServer()
-            except Exception as exc:
-                log.error("Failed to stop JACK: %s", exc)
-                self.status = str(exc)
+        if self.jackctl and self.jack_status.get('is_started'):
+            self.jackctl.stop_server()
             self.check_jack_status()
 
 
 def main():
     """Main function to be used when called as a script."""
+    from dbus.mainloop.glib import DBusGMainLoop
     logging.basicConfig(
         level=logging.DEBUG if '-v' in sys.argv[1:] else logging.INFO,
         format="[%(name)s] %(levelname)s: %(message)s")
+    DBusGMainLoop(set_as_default=True)
     JackSelectApp()
     return Gtk.main()
 
