9fc574000d3cf6ad760a363b3f490635f6040ec1
Christopher Arndt <chris@chrisarndt.de>
2019-12-01 13:58:31 +0100

    Add docstrings to jack-select D-BUS service methods
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- jackselect/jackselect_service.py
+++ jackselect/jackselect_service.py
@@ -27,30 +27,36 @@ class JackSelectService(dbus.service.Object):
 
     @dbus.service.method(dbus_interface=DBUS_INTERFACE, out_signature='i')
     def GetPid(self):
+        """Get the PID of the currently running JACK-Select process."""
         log.debug("DBus client requested PID.")
         return os.getpid()
 
     @dbus.service.method(dbus_interface=DBUS_INTERFACE)
     def Exit(self):
+        """Exit JACK-Select."""
         log.debug("DBus client requested application exit.")
         self.app.quit()
 
     @dbus.service.method(dbus_interface=DBUS_INTERFACE)
     def OpenMenu(self):
+        """Open the JACK-Select popup menu."""
         log.debug("DBus client requested opening menu.")
         self.app.open_menu()
 
     @dbus.service.method(dbus_interface=DBUS_INTERFACE, in_signature='s')
     def ActivatePreset(self, preset):
+        """Activate the JACK configuration preset with the given name."""
         log.debug("DBus client requested activating preset '%s'." % preset)
         self.app.activate_preset(preset=preset)
 
     @dbus.service.method(dbus_interface=DBUS_INTERFACE)
     def ActivateDefaultPreset(self):
+        """Activate the default JACK configuration preset."""
         log.debug("DBus client requested activating default preset.")
         self.app.activate_default_preset()
 
     @dbus.service.method(dbus_interface=DBUS_INTERFACE)
     def StopJackServer(self):
+        """Stop the JACK server."""
         log.debug("DBus client requested stopping JACK server.")
         self.app.stop_jack_server()
