c1af30dae3166c0b6fe7cf6049716a9c89c8ff54
Christopher Arndt <chris@chrisarndt.de>
2021-01-15 08:44:38 +0100

    Update supported Python versions
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- setup.py
+++ setup.py
@@ -66,10 +66,10 @@ setuptools.setup(
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Environment :: X11 Applications :: GTK',
         'Topic :: Multimedia :: Sound/Audio'
     ],
