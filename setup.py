34efb723a622ecf91c22606c3baff2c2f4258ffb
Christopher Arndt <chris@chrisarndt.de>
2022-01-17 17:58:10 +0100

    docs: add clasifier for Python 3.10 support to package meta data
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Environment :: X11 Applications :: GTK',
         'Topic :: Multimedia :: Sound/Audio'
     ],
